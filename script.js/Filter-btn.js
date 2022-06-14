941b721d92289c60ddaa56132e403319df1bc5d4
Edwin Echeverria <edwindanielnow@gmail.com>
2022-06-14 15:49:07 -0400

    new changes

--- script.js/Filter-btn.js
+++ script.js/Filter-btn.js
@@ -38,7 +38,9 @@ function carBtn() {
     x.classList.toggle("hidden-eight");
 }
 
-let count = Number(localStorage.getItem("count"));  
+let count = 0;
+let countTwo = 0;
+ 
 
 let cel = [
     {id: 1, brand: `SAMSUNG`, model: `Galaxy S52`, price: 5200, img:"./images/image 1.jpg"},
@@ -50,10 +52,68 @@ let cel = [
     {id: 7, brand: `SAMSUNG`, model: `Galaxy S21`, price: 16500, img:"./images/image 16.jpg"}    
 ];
 
+// function generateCardCars(arrayCel) {
+//     let html="";
+//     for(let i = 0; i < arrayCel.length; i++) {
+//         html += ` <div class="car-grid d-grid">
+//                     <div class="car-text">
+//                         <p class="text-small-car color-light">${arrayCel[i].brand}</p>
+//                         <p class="bold text-small padding-01">${arrayCel[i].model}</p>
+//                     </div>
+//                     <div class="car-count padding-05">
+//                         <form action="#">
+//                             <label for="lang"></label>
+//                             <select name="languages" id="count" class="select bold">
+//                                 <option value="0">1</option`
+//                                 const container = document.getElementById("container");
+//                                 container.innerHTML += html;
+//     }
+// }
+
+function one(arrayCel,cb) {
+    let result = [];
+    for(let i = 0; i < arrayCel.length; i++) {
+        let arr = cb(arrayCel[i]);
+        if(arr){
+            result.push(arrayCel[i])
+        }
+    }
+    return result;
+}
+
+function as(element) { 
+    const input = document.getElementById("1")
+    if(count < 1) {
+        count = 1
+        return element.id == input.value
+    }
+}
+
+function productOne() {
+    let arr = one(cel, as);
+    generateCardCars(arr)
+}
+
+
+function es(element) {
+    const input = document.getElementById("2")
+    if(countTwo < 1) {
+        countTwo = 1
+        console.log(countTwo)
+        return element.id == input.value
+    }
+}
+
+function productTwo() {
+    let er = one(cel, es);
+    generateCardCars(er)
+}
+
+
 function generateCardCars(celArray) {
     let html = '';
     for(let i = 0; i < celArray.length; i++) {
-        html +=` <div class="car-grid d-grid">
+        html =` <div class="car-grid d-grid">
                     <div class="img-cel-mini">
                         <img src="${celArray[i].img}" alt="">
                     </div>
@@ -73,12 +133,15 @@ function generateCardCars(celArray) {
                 </div>` 
     }
     const container = document.getElementById("carritoC");
-    container.innerHTML = html;
+    container.innerHTML += html;
 }
-generateCardCars(cel);
 
 function addCounter(){
     count += 1;
     localStorage.setItem("count", count);
-    generateCardCars();
+}
+
+function addCounterTwo(){
+    countTwo += 1;
+    localStorage.setItem("countTwo", countTwo);
 }
